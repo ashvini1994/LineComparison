e5d07ee3569ec15847a7d59835822cfa51dd87ef
Ashu <ashusarnaik11@gmail.com>
2021-11-24 20:30:42 +0530

    find length

--- LineComparison1/LineComparison1/Program.cs
+++ LineComparison1/LineComparison1/Program.cs
@@ -10,7 +10,20 @@ namespace LineComparison1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation problem");
+            Console.WriteLine("Line Camparison Computation problem");
+            double x1, y1, x2, y2;
+            double lenght = 0;
+            Console.WriteLine("Enter Starting point of line x1 and y1 ordinate");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter End point of line x2 and y2 ordinate");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+            double powx = Math.Pow(x2 - x1, 2);
+            double powy = Math.Pow(y2 - y1, 2);
+            lenght = Math.Sqrt(powx + powy);
+            Console.WriteLine("Lenght of line is " + lenght);
+            Console.ReadKey();
         }
     }
 }
